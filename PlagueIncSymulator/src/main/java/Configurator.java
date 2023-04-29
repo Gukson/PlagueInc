10d01cd5af3380ad934b7742bd4e99840366e120
Gukson <75785907+Gukson@users.noreply.github.com>
2023-04-29 21:54:12 +0200

    Created basic class and started working with Country class

---
+++ PlagueIncSymulator/src/main/java/Configurator.java
@@ -0,0 +1,7 @@
+public class Configurator {
+
+    public void Startup(){
+
+    }
+
+}
---
+++ PlagueIncSymulator/src/main/java/Country.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Country {
+    public boolean infected = false;
+    private String Country_name;
+    private int population;
+    private float temperature;
+    private String  klimat;
+    private ArrayList<Country> neightboors;
+
+
+}
