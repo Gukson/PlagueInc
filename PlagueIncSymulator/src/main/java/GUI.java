b5fcb2c819aedeb087ced302aeee2cadfcfb65d8
Gukson <75785907+Gukson@users.noreply.github.com>
2023-05-08 11:57:06 +0200

    moved code form GUI class to Configurator class

--- PlagueIncSymulator/src/main/java/GUI.java
+++ PlagueIncSymulator/src/main/java/GUI.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
@@ -5,52 +6,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GUI {
-    private ArrayList<Country> countries = new ArrayList<>();
-    public GUI(){
-        readInfoAboutCountries("PlagueIncSymulator/Data/countiries_data.csv");
-        System.out.println("XD");
-        int population = calculateWorldPopulation();
-        World.setPopulation(population);
 
-    }
-    private void readInfoAboutCountries(String filename) {
-        try {
-            Scanner scanner = new Scanner(new File(filename));
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                if (line.startsWith("KRAJ")) {
-                    continue;
-                }
-                String[] fields = line.split(";");
-                String name = fields[0];
-                String populationStr = fields[1].replaceAll("\\s+", "");
-                int population = Integer.parseInt(populationStr);
-                String avgTempStr = fields[2].replaceAll("[^\\d.-]+", "");
-                double avgTemp = Double.parseDouble(avgTempStr);
-                String climate = fields[3];
-                countries.add(new Country(name, population, avgTemp, climate));
-            }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    private int calculateWorldPopulation(){
-        int population = 0;
-        for (Country country :countries){
-            population += country.getPopulation();
-        }
-        return population;
-    }
-    public static void main(String[] args) {
-        GUI gui = new GUI();
-        int worldPopulation = World.getPopulation();
-        System.out.println("WORLD POPULATION " + worldPopulation);
-        for (Country country : gui.countries){
-            System.out.println(country.getName() + "  POPULATION: " + country.getPopulation());
-        }
-        System.out.println("Hello");
-    }
+
 }
 
 
