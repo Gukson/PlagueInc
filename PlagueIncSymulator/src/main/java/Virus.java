44af55f07a45e8f687e3eb561669a9eab014b91d
Gukson <75785907+Gukson@users.noreply.github.com>
2023-06-04 02:47:23 +0200

    Flight&Ship System begin

--- PlagueIncSymulator/src/main/java/Virus.java
+++ PlagueIncSymulator/src/main/java/Virus.java
@@ -4,18 +4,27 @@ public class Virus {
 
     public double cheanseForDeath;
     public double cheanseForInfection;
-    public int virusPoints;
-
+    private int virusPoints;
     public int difficulty;
+    private boolean airplane, ship;
 
     public Virus(String Name){
         this.virus_name = Name;
         cheanseForDeath = 0;
         cheanseForInfection = 0.1;
         virusPoints = 0;
+        airplane = false;
+        ship = false;
     }
 
     public void addPoint(){virusPoints++;}
+    public int getPoints(){return virusPoints;}
+    public String getName(){return virus_name;}
+    public void setOnAirplane(){airplane=true;}
+    public void setOnShip(){ship=true;}
+
+
+
 
 
 
